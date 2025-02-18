f023caa6ad2e3c70e236f66a408beabaca9e9456
subhajit100 <asubhajit35@gmail.com>
2025-02-18 15:48:13 +0530

    add categories data

---
+++ helpers/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        id: 1, 
+        name: "Restroom",
+        icon: "/images/restroom.png"
+    },
+    {
+        id: 2, 
+        name: "Retail",
+        icon: "/images/retail.png"
+    },
+    {
+        id: 3, 
+        name: "Parking",
+        icon: "/images/parking.png"
+    },
+    {
+        id: 4, 
+        name: "Spiritual",
+        icon: "/images/spiritual.png"
+    },
+]
+
+module.exports = {categories}
